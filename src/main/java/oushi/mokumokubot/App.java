106efed2a99e3e4906aafd62d03ab90e8a4e7a45
ttymryo <106653745+ttymryo@users.noreply.github.com>
2022-10-12 18:31:25 +0900

    修正

--- src/main/java/oushi/mokumokubot/App.java
+++ src/main/java/oushi/mokumokubot/App.java
@@ -1,20 +1,32 @@
 package oushi.mokumokubot;
 import javax.security.auth.login.LoginException;
 
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
 
 public class App 
 {
-	private static JDA jda = null;
+	private final ShardManager shardManager;
     private static final String BOT_TOKEN = (System.getenv("TOKEN"));
     
-    public static void main( String[] args ) throws LoginException {
-    		jda = JDABuilder.createDefault(BOT_TOKEN, GatewayIntent.GUILD_MESSAGES)
-                    .setRawEventsEnabled(true)
-                    .setActivity(Activity.playing("もくもく監視中"))
-                    .build();
+    public App() throws LoginException {
+    	DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(BOT_TOKEN);
+    	builder.setStatus(OnlineStatus.ONLINE);
+    	builder.setActivity(Activity.watching("もくもく"));
+    	shardManager = builder.build();
+    }
+    
+    public ShardManager getShardManager() {
+    	return shardManager;
+    }
+    
+    public static void main( String[] args ) {
+    		try {
+    			App bot = new App();
+    		} catch (LoginException e) {
+    			System.out.println("ERROR: provided bot token is invalid!");
+    		}
     }
 }
