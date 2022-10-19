872accb9cf348d02ce4f93bf06bb79bd9a9a09dd
ttymryo <106653745+ttymryo@users.noreply.github.com>
2022-10-19 22:51:34 +0900

    修正

--- src/main/java/oushi/mokumokubot/listeners/EventListener.java
+++ src/main/java/oushi/mokumokubot/listeners/EventListener.java
@@ -19,13 +19,28 @@ public class EventListener extends ListenerAdapter{
 		((MessageChannel) event.getGuild().getDefaultChannel()).sendMessage(message).queue();
 		}
 
-	@Override
-	public void onMessageReceived(MessageReceivedEvent event) {
-		String message = event.getMessage().getContentRaw();
-		if (message.contains("ping")) {
-			((MessageChannel) event.getChannel()).sendMessage("pong").queue();
-		}
-	}
+//	@Override
+//	public void onMessageReceived(MessageReceivedEvent event) {
+//		String message = event.getMessage().getContentRaw();
+//		if (message.contains("ping")) {
+//			((MessageChannel) event.getChannel()).sendMessage("pong").queue();
+//		}
+//	}
 	
+	@Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.getAuthor().isBot()) return;
+        // We don't want to respond to other bot accounts, including ourself
+        net.dv8tion.jda.api.entities.Message message = event.getMessage();
+        String content = message.getContentRaw(); 
+        // getContentRaw() is an atomic getter
+        // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
+        if (content.equals("!ping"))
+        {
+            MessageChannel channel = event.getChannel();
+            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
+        }
+    }
 	
 }
